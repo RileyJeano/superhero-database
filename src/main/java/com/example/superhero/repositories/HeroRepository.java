3776c355a6acb2b212dc83fc067950d752dd4cf3
Joe Rudy <joe.rudy@sbcglobal.net>
2018-11-14 14:55:35 -0500

    update

--- src/main/java/com/example/superhero/repositories/HeroRepository.java
+++ src/main/java/com/example/superhero/repositories/HeroRepository.java
@@ -4,12 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.superhero.models.Hero;
 
-<<<<<<< HEAD
-public interface HeroRepository extends CrudRepository <Hero, Long> {
-=======
+
 
 public interface HeroRepository extends CrudRepository<Hero, Long> {
 
->>>>>>> 461d278118f71d38303fd7f178e07cd5589f0bb0
 
 }
