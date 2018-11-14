acff972c928769fea49ed89a3837af0f783f90f1
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-14 11:55:06 -0500

    fixed exstranious import

--- src/main/java/com/example/superhero/controllers/ApiController.java
+++ src/main/java/com/example/superhero/controllers/ApiController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.wecancodeit.soundscapes_collection.models.Album;
+
 
 import com.example.superhero.models.Bottom;
 import com.example.superhero.models.Hair;
